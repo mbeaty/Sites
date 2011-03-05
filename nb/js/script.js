bc66c2be4c5679f0d3556c39e711456e4ae954f4
Mark Beaty <mabonline@comcast.net>
2011-03-05 00:27:23 -0700

    Make IWON header clickable. Add Contact Us link in rules list. Setup click handlers for form buttons.

--- nb/js/script.js
+++ nb/js/script.js
@@ -1,6 +1,22 @@
 /* Author: 
 
 */
+$(document).ready(function(){
+	// Make IWON header clickable
+	$('#hdr-primary').click(function(){
+	     window.location='http://iwon.com';
+	     return false;
+	});
+	// Setup form buttons click events
+	$('#create-btn').click(function(){
+		$('#DefCreateButton').click();
+		return false;
+	});
+	$('#signin-btn').click(function(){
+		$('#DefSigninButton').click();
+		return false;
+	});
+});
 
 
 
